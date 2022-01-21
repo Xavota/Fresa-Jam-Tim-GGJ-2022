1d6f2f0856cd1c5185e4b4e68188fd16dc2765f6
Xavota <57270722+Xavota@users.noreply.github.com>
2022-01-21 00:52:22 -0600

    Proyecto subido

---
+++ Source/ShadowLight/ShadowLightGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShadowLightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADOWLIGHT_API AShadowLightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
